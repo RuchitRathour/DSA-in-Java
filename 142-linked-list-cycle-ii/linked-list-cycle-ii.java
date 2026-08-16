b2672cef9c249eee8eb498299adedeb9d00ae5da
Ruchit Rathour <ruchitrathour@gmail.com>
2026-08-16 23:05:30 +0530

    Time: 0 ms (100.00%) | Memory: 46.4 MB (87.26%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -11,35 +11,62 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-       if (head == null || head.next == null) return null;
+    //    if (head == null || head.next == null) return null;
 
-        ListNode slow = head;
-        ListNode fast = head;
+    //     ListNode slow = head;
+    //     ListNode fast = head;
 
-        // Step 1: Detect cycle using slow and fast pointers
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
+    //     // Step 1: Detect cycle using slow and fast pointers
+    //     while (fast != null && fast.next != null) {
+    //         slow = slow.next;
+    //         fast = fast.next.next;
 
-            // Cycle found
-            if (slow == fast) {
-                break;
+    //         // Cycle found
+    //         if (slow == fast) {
+    //             break;
+    //         }
+    //     }
+
+    //     // If no cycle
+    //     if (fast == null || fast.next == null) {
+    //         return null;
+    //     }
+
+    //     // Step 2: Find the start of the cycle
+    //     slow = head;  // move slow to head
+
+    //     while (slow != fast) {
+    //         slow = slow.next;
+    //         fast = fast.next; // both move one step
+    //     }
+
+    //     return slow;  // this is the start of the cycle 
+
+
+    ListNode slow = head;
+    ListNode fast = head;
+
+    // Phase 1: Cycle detect
+    while (fast != null && fast.next != null) {
+
+        slow = slow.next;
+        fast = fast.next.next;
+
+        if (slow == fast) {
+
+            // Phase 2: Find cycle start
+            slow = head;
+
+            while (slow != fast) {
+                slow = slow.next;
+                fast = fast.next;
             }
+
+            return slow;
         }
+    }
 
-        // If no cycle
-        if (fast == null || fast.next == null) {
-            return null;
-        }
+    return null;
 
-        // Step 2: Find the start of the cycle
-        slow = head;  // move slow to head
-
-        while (slow != fast) {
-            slow = slow.next;
-            fast = fast.next; // both move one step
-        }
-
-        return slow;  // this is the start of the cycle 
     }
 }
